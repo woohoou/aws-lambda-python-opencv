a814d8dfcefe0bdcbe307eba1793ad210c7ab60f
Heber Uriegas <heber.fernando@gmail.com>
2019-03-10 22:20:00 -0600

    Set to python 3.6

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Setting up build env
+sudo yum install -y https://centos6.iuscommunity.org/ius-release.rpm
 sudo yum update -y
-sudo yum install -y git cmake gcc-c++ gcc python37u python37u-libs python37u-devel python37u-pip chrpath
+sudo yum install -y git cmake gcc-c++ gcc python36u python36u-libs python36u-devel python36u-pip chrpath
 mkdir -p lambda-package/cv2 build/numpy
+
+python3.6 -m venv venv
+. venv/bin/activate
 
 # Build numpy
 pip install --install-option="--prefix=$PWD/build/numpy" numpy
-cp -rf build/numpy/lib64/python3.7/site-packages/numpy lambda-package
+cp -rf build/numpy/lib64/python3.6/site-packages/numpy lambda-package/numpy
 
 # Build OpenCV 3.4
 (
@@ -16,26 +20,27 @@ cp -rf build/numpy/lib64/python3.7/site-packages/numpy lambda-package
 	git clone https://github.com/Itseez/opencv.git
 	cd opencv
 	git checkout 3.4.0
-	cmake										\
-		-D CMAKE_BUILD_TYPE=RELEASE				\
-		-D WITH_TBB=ON							\
-		-D WITH_IPP=ON							\
-		-D WITH_V4L=ON							\
-		-D ENABLE_AVX=ON						\
-		-D ENABLE_SSSE3=ON						\
-		-D ENABLE_SSE41=ON						\
-		-D ENABLE_SSE42=ON						\
-		-D ENABLE_POPCNT=ON						\
-		-D ENABLE_FAST_MATH=ON					\
-		-D BUILD_EXAMPLES=OFF					\
-		-D BUILD_TESTS=OFF						\
-		-D BUILD_PERF_TESTS=OFF					\
-		-D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"	\
-		.
+	mkdir build && cd build
+	cmake
+		-D CMAKE_BUILD_TYPE=RELEASE \
+		-D WITH_TBB=ON \
+		-D WITH_IPP=ON \
+		-D WITH_V4L=ON \
+		-D ENABLE_AVX=ON \
+		-D ENABLE_SSSE3=ON \
+		-D ENABLE_SSE41=ON \
+		-D ENABLE_SSE42=ON \
+		-D ENABLE_POPCNT=ON \
+		-D ENABLE_FAST_MATH=ON \
+		-D BUILD_EXAMPLES=OFF \
+		-D BUILD_TESTS=OFF \
+		-D BUILD_PERF_TESTS=OFF \
+		-D PYTHON3_NUMPY_INCLUDE_DIRS="$NUMPY" \
+	..
 	make -j`cat /proc/cpuinfo | grep MHz | wc -l`
 )
-cp build/opencv/lib/cv2.so lambda-package/cv2/__init__.so
-cp -L build/opencv/lib/*.so.3.4 lambda-package/cv2
+cp build/opencv/build/lib/python3/cv2.cpython-36m-x86_64-linux-gnu.so lambda-package/cv2/__init__.so
+cp -L build/opencv/build/lib/*.so.3.4 lambda-package/cv2
 strip --strip-all lambda-package/cv2/*
 chrpath -r '$ORIGIN' lambda-package/cv2/__init__.so
 touch lambda-package/cv2/__init__.py
